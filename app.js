b903ab51178757010cf50f514aabd8b4135c2a04
paulaolivereira2 <paula.oliveira2@escolar.ifrn.edu.br>
2026-08-20 13:28:20 -0300

    modificacao

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 alert("ola mundo!");
 
-var nasc = 2007;
-let nome = "Ana";
-const viva = true;
-let altura = 1.70;
+var nasc = 2007;      
+let nome = "Ana";   
+const viva = true;     
+let altura = 1.70;     
 
-if(viva){
-    let saudacao = "ola" + nome + "!";
-    let mensagem = 'altura: ${altura}m | idade: ${2025-nasc}';
-    alert(saudacao+ '\n' + mensagem);
+if (viva) {
+    let saudacao = "Olá, " + nome + "!";
+    let mensagem = `Altura: ${altura}m | Idade: ${2025 - nasc}`;
+    alert(saudacao + '\n' + mensagem);
 } else {
-    alert("voce não está viva!");
+    alert("Você não está viva!");
 }
