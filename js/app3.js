859f2f9f7494ed439907070d3b737ae3a0bd8dd8
paulaolivereira2 <paula.oliveira2@escolar.ifrn.edu.br>
2026-08-26 17:53:10 -0300

    sla

---
+++ js/app3.js
@@ -0,0 +1,15 @@
+let nasc = 1991;
+let nome = "Ana";
+let viva = true;
+let altura = 1.70;
+let sexo;
+let peso = null;
+let imc = "peso"*10;
+
+alert(`Ano de nascimento - tipo: ${type(nasc)}`)
+alert(`Nome - tipo: ${type(nome)}`)
+alert(`Viva - tipo: ${type(viva)}`)
+alert(`Altura - tipo: ${type(altura)}`)
+alert(`Sexo - tipo: ${type(sexo)}`)
+alert(`Peso - tipo: ${type(peso)}`)
+alert(`IMC - tipo: ${type(imc)}`)
