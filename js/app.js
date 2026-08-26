10b98aecd1010b9c9800eccdd2a2de47acbffbea
paulaolivereira2 <paula.oliveira2@escolar.ifrn.edu.br>
2026-08-26 17:36:21 -0300

    alteracao

--- js/app.js
+++ js/app.js
@@ -7,14 +7,18 @@ function calcIdade(ano=2026){
     let menor;
     if (idade < 18){
          menor = true;
+         var podebeber = false;
     }else{
         menor = false;
+        var podebeber = true;
     }
-        alert(`${nome} é menor de idade? ${menor}\nIdade: ${idade}`)
+        alert(`${nome} é menor de idade? ${menor}\nIdade: ${idade}\nPode beber:${podebeber}`)
         return idade;
 }
 
 calcIdade();
+/* var vaza a variavel dentro do escopo da mesma funcao
+alert(`pode beber: ${podebeber}`);
 /*
 alert(`Fora da funcao: Idade ${idade}`);
 Erro pois a variavel let nao existe fora do escopo dele
