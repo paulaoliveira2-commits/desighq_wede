6e07fbfe1b5c0a739b3d46753b13e3b0965a986e
paulaolivereira2 <paula.oliveira2@escolar.ifrn.edu.br>
2026-08-26 17:04:26 -0300

    alterei

--- js/app.js
+++ js/app.js
@@ -4,8 +4,13 @@ const viva = true;
 
 function calcIdade(ano=2026){
     let idade = ano - nasc;
-    alert(`dentro de função - idade ${idade}`)
-    return idade;
+    if (idade < 18){
+         menor = true;
+    }else{
+        menor = false;
+    }
+        alert(`${nome} é menor de idade? ${menor}\nIdade: ${idade}`)
+        return idade;
 }
 
 calcIdade();
