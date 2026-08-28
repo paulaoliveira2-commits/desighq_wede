96e6ec4f6c8d1168293369a6b2a885674a461901
paulaolivereira2 <paula.oliveira2@escolar.ifrn.edu.br>
2026-08-28 13:11:16 -0300

    sla

--- js/app3.js
+++ js/app3.js
@@ -6,10 +6,10 @@ let sexo;
 let peso = null;
 let imc = "peso"*10;
 
-alert(`Ano de nascimento - tipo: ${type(nasc)}`)
-alert(`Nome - tipo: ${type(nome)}`)
-alert(`Viva - tipo: ${type(viva)}`)
-alert(`Altura - tipo: ${type(altura)}`)
-alert(`Sexo - tipo: ${type(sexo)}`)
-alert(`Peso - tipo: ${type(peso)}`)
-alert(`IMC - tipo: ${type(imc)}`)
+alert(`Ano de nascimento - tipo: ${typeof(nasc)}`);
+alert(`Nome - tipo: ${typeof(nome)}`);
+alert(`Viva - tipo: ${typeof(viva)}`);
+alert(`Altura - tipo: ${typeof(altura)}`);
+alert(`Sexo - tipo: ${typeof(sexo)}`);
+alert(`Peso - tipo: ${typeof(peso)}`);
+alert(`IMC - tipo: ${typeof(imc)}`);
