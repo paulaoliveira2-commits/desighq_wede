94a8a0dd81969adbd011584d076fdc7a81da93ab
paulaolivereira2 <paula.oliveira2@escolar.ifrn.edu.br>
2026-09-04 13:16:25 -0300

    mudanças

---
+++ js/app2.js
@@ -0,0 +1,22 @@
+let dia = prompt ("Escolha um dia da semana\nSendo 1:Domingo - 7:Sábado")
+switch(dia){
+    case 1: alert("Voce escolheu dormingo");
+    case 2: alert("Voce escolheu segunda");
+    case 1: alert("Voce escolheu dormingo");
+}
+
+if (dia <=0 || dia >=8){
+    alert("Dia inválido");
+}else if(dia == 1){
+    alert("Voce escolheu dormingo")
+}else if(dia == 2){
+    alert("Voce escolheu segunda")
+}else if(dia == 3){
+    alert("Voce escolheu terça")
+}else if(dia == 4){
+    alert("Voce escolheu quarta")
+}else if(dia == 5){
+    alert("Voce escolheu quinta ")
+}else if(dia == 6){
+    alert("Voce escolheu sexta")
+}
